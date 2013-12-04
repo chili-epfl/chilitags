31785c454e3dcea1c7733fe1174ac8d18c806212
Quentin Bonnard <quentin.bonnard+github@gmail.com>
2013-12-04 12:46:14 +0100

    Updated the tests

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -53,14 +53,6 @@ TEST(Integration, Snapshots) {
         {"stills/640x480/nao05.jpg",       {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19}},
     };
 
-    // The game is to lower these numbers
-#ifdef OPENCV3
-	const int tExpectedFalseNegatives = 3;
-#else
-	const int tExpectedFalseNegatives = 5;
-#endif
-    const int tExpectedFalsePositives = 0;
-
     // Initialise the data path with en empty modulename,
     // to get the data from the root of the test data path
     cvtest::TS::ptr()->init("");
@@ -74,8 +66,6 @@ TEST(Integration, Snapshots) {
     // (Chilitag's constructor takes care of that)
     for (int i = 0; i<1024; ++i) Chilitag tTag(i);
 
-    int tActualFalseNegatives = 0;
-    int tActualFalsePositives = 0;
     for (const auto & tTestCase : tTestMatrix) {
         string tPath = string(cvtest::TS::ptr()->get_data_path())+tTestCase.first;
         tImage = cv::imread(tPath);
@@ -86,30 +76,6 @@ TEST(Integration, Snapshots) {
                 tDetectChilitags.update();
                 runs_duration.push_back(((double)cv::getTickCount() - tStartCount)*1000/cv::getTickFrequency());
             }
-
-            //cout << "Missing markers for " << tTestCase.first << ": ";
-            // We consider everything false positive,
-            // and remove the expected ones later
-            auto tExpectedTagIt = tTestCase.second.begin();
-            for (int i = 0; i<1024; ++i) {
-                // No persistence
-                Chilitag tTag(i, 0);
-                if (*tExpectedTagIt == i) {
-                    if (!tTag.isPresent()) {
-                        ++tActualFalseNegatives;
-                        //cout << i << " ";
-                    }
-                    ++tExpectedTagIt;
-                }
-                else if (tTag.isPresent()) {
-                    ++tActualFalsePositives;
-                    ADD_FAILURE() << "Falsely positive detection\n"
-                        << "    File: " << tTestCase.first << "\n"
-                        << "     Tag: " << i << "\n";
-                }
-            }
-            //cout << endl;
-
         }
         else {
             ADD_FAILURE()
@@ -123,13 +89,6 @@ TEST(Integration, Snapshots) {
     cout << "Benchmark results, on " << ITERATIONS << " iterations:" << endl;
     cout << "\t- Time to update markers: " << mean(runs_duration) << "ms" << endl;
     cout << "\t- Standard deviation: " << sigma(runs_duration) << "ms" << endl;
-
-
-    EXPECT_LE(tExpectedFalseNegatives, tActualFalseNegatives)
-        << "It looks like you actually improved the detection here\n"
-        << "Please review and update this test case to doube check\n";
-    EXPECT_EQ(tExpectedFalseNegatives, tActualFalseNegatives);
-    EXPECT_EQ(tExpectedFalsePositives, tActualFalsePositives);
 }
 
 CV_TEST_MAIN(".")
--- test/test-metadata.hpp
+++ test/test-metadata.hpp
@@ -14,17 +14,24 @@ namespace TestMetadata {
 		}
 	};
 
-
 	// The string is the filename of an image,
 	// the first list correspond to the ids of tags shown on the image,
 	// and the second list to the ids of the tags that are undetected.
 	std::vector< TestCase > all = {
 		{"stills/1600x1200/thesis01.jpg",
 			{369,370,371,372,373,374,375,377,378,379,380,381,382,383,384,385,386,394,395,396,397,398,399,400,401,402,405,406,407,408,409},
+#ifdef EXPERIMENTAL_LSD
+			{369}},
+#else
 			{}},
+#endif
 		{"stills/1600x1200/thesis02.jpg",
 			{588,589,590,591,592,593,594,595,596,597,598,599,1000},
+#ifdef EXPERIMENTAL_LSD
+			{}},
+#else
 			{597,}},
+#endif
 		{"stills/1600x1200/thesis03.jpg",
 			{576,577,588,589,590,591,593,594,595,1023},
 			{}},
@@ -33,7 +40,11 @@ namespace TestMetadata {
 			{}},
 		{"stills/640x480/severin01.jpg",
 			{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19},
+#ifdef EXPERIMENTAL_LSD
+			{}},
+#else
 			{1,13,}},
+#endif
 		{"stills/640x480/severin02.jpg",
 			{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19},
 			{}},
@@ -45,13 +56,21 @@ namespace TestMetadata {
 			{14,}},
 		{"stills/640x480/severin05.jpg",
 			{0,1,    4,5,6,7,          12,13,14,      17,18   },
+#ifdef EXPERIMENTAL_LSD
+			{5,6,7,12,13,14,18,}},
+#else
 			{5,7,12,13,18,}},
+#endif
 		{"stills/640x480/severin06.jpg",
 			{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19},
 			{}},
 		{"stills/640x480/nasty01.jpg",
 			{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19},
+#ifdef EXPERIMENTAL_LSD
+			{1,8,16,}},
+#else
 			{1,13,}},
+#endif
 		{"stills/640x480/nao01.jpg",
 			{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19},
 			{}},
@@ -66,10 +85,18 @@ namespace TestMetadata {
 			{}},
 		{"stills/640x480/nao05.jpg",
 			{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19},
+#ifdef EXPERIMENTAL_LSD
+			{}},
+#else
 			{1,}},
+#endif
 		{"stills/640x480/nao06.jpg",
 			{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19},
+#ifdef EXPERIMENTAL_LSD
+			{0,4,}},
+#else
 			{0,1,4,6,8,15,}},
+#endif
 		{"stills/640x480/occlusion01.jpg",
 			{  1                                     ,17      },
 			{1,17,}},
@@ -78,28 +105,56 @@ namespace TestMetadata {
 			{1,17,}},
 		{"stills/320x240/nao01.png",
 			{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19},
+#ifdef EXPERIMENTAL_LSD
+			{1,2,5,6,8,9,10,11,12,13,15,16,17,19,}},
+#else
 			{1,2,5,6,8,9,10,11,12,13,14,15,16,17,19,}},
+#endif
 		{"stills/320x240/nao02.png",
 			{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19},
+#ifdef EXPERIMENTAL_LSD
+			{1,2,4,5,6,7,9,11,12,13,14,15,16,17,19,}},
+#else
 			{1,2,4,5,6,7,8,9,11,13,14,15,16,17,18,19,}},
+#endif
 		{"stills/320x240/nao03.png",
 			{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19},
+#ifdef EXPERIMENTAL_LSD
+			{2,7,}},
+#else
 			{2,5,7,10,14,16,18,}},
+#endif
 		{"stills/320x240/nao04.png",
 			{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19},
+#ifdef EXPERIMENTAL_LSD
+			{0}},
+#else
 			{3,6,14,}},
+#endif
 		{"stills/320x240/nao05.png",
 			{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19},
 			{0,1,2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,19,}},
 		{"stills/320x240/nao06.png",
 			{46, 50, 51, 55, 59, 62},
+#ifdef EXPERIMENTAL_LSD
+			{46,50,51,55,59,62,81,}},
+#else
 			{46,50,51,59,62,81,}},
+#endif
 		{"stills/320x240/nao07.png",
 			{46, 50, 51, 55, 59, 62},
+#ifdef EXPERIMENTAL_LSD
+			{46,50,51,55,59,62,81,}},
+#else
 			{46,50,55,59,62,81,}},
+#endif
 		{"stills/320x240/nao08.png",
 			{50, 51, 55, 62},
+#ifdef EXPERIMENTAL_LSD
+			{50,51,55,62,}},
+#else
 			{50,51,62,}},
+#endif
 	};
 }
 
