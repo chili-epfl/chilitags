dd89d757c36f05250f3230d9373042b943f13f9a
Aaron Roller <aaron.roller@automodality.com>
2019-12-29 07:51:55 -0800

    BB-758 requires changelog.

--- .github/actions/package@local/entrypoint.sh
+++ .github/actions/package@local/entrypoint.sh
@@ -5,4 +5,11 @@ set -e # fail on error
 apt-get -y update
 apt-get -y install javahelper # required for debhelper
 fakeroot debian/rules clean
+
+project_name="$(basename $(git config remote.origin.url |sed "s/\.git$//"))"
+timestamp=$(date +%Y%m%d%H%M%S)
+version="$timestamp"
+echo "$project_name $version"
+echo "chilitags (0.0.0-12345) unstable; urgency=low" > debian/changelog
+
 fakeroot debian/rules binary
