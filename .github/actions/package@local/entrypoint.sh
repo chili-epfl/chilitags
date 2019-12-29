278334af5984f5f4000140a4b4359e40ab018a32
Aaron Roller <aaron.roller@automodality.com>
2019-12-29 12:52:07 -0800

    BB-758 package is generated with version

--- .github/actions/package@local/entrypoint.sh
+++ .github/actions/package@local/entrypoint.sh
@@ -2,28 +2,27 @@
 
 set -e # fail on error
 
+#see action.yml for inputs
 
-author=$1
-github_ref=$2
+author=$1 # who triggered the build
+github_ref=$2 # the branch, tag or pull request path
 
-echo "author=$author"
-echo "github_ref=$github_ref"
-
-source /opt/ros/kinetic/setup.bash
+source /opt/ros/kinetic/setup.bash # to find opencv
 
 apt-get -y update
 apt-get -y install javahelper # required for debhelper
 
 project_name="$(basename $(git config remote.origin.url |sed "s/\.git$//"))"
 timestamp=$(date +%Y%m%d%H%M%S)
-version="$timestamp"
-echo "$project_name $version"
-echo "chilitags (0.0.0-12345) unstable; urgency=low" > debian/changelog
+build_number=$(echo $github_ref | cut -d'/' -f3) # refs/pull/2/merge is actuall pr# until build numbers are available
+version="$build_number-$author-$timestamp"
+echo "$version"
+echo "chilitags ($version) unstable; urgency=low" > debian/changelog
 
 
-fakeroot debian/rules clean
-fakeroot debian/rules binary
+fakeroot debian/rules clean #ensures no residue
+fakeroot debian/rules binary #performs the package
 
-artifact_filename=$(ls .. | grep $project_name)
+artifact_filename=$(ls .. | grep $project_name) #the package is generated in base directory
 
-echo ::set-output name=artifact-path::$artifact_filename
+echo ::set-output name=artifact-path::$artifact_filename  #action syntax for passing out variables
