1ad3d47154f691cc239614950330027112f16041
Aaron Roller <aaron.roller@automodality.com>
2019-12-29 17:45:29 -0800

    BB-758 mv syntax error

--- .github/actions/package@local/entrypoint.sh
+++ .github/actions/package@local/entrypoint.sh
@@ -26,5 +26,5 @@ fakeroot debian/rules binary #performs the package
 # the file is generated in the home dir, but we are in the build dir still. move it to staging for sharing
 artifact_filename=$(ls .. | grep $project_name) #the package is generated in base directory
 artifact_path="$staging_dir/$artifact_filename"
-mv "../$artifact_filename $artifact_path"
+mv "../$artifact_filename" $artifact_path
 echo ::set-output name=artifact-path::$artifact_path  #action syntax for passing out variables
